239e995986c8ce4735c04be463683bbc7f0f1d5e
Simon Golay <simon.golay@ceff.ch>
2024-11-08 08:36:30 +0100

    player class

--- PongClient/Form1.Designer.cs
+++ PongClient/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pb = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pb).BeginInit();
+            SuspendLayout();
+            // 
+            // pb
+            // 
+            pb.BackColor = SystemColors.GradientActiveCaption;
+            pb.Dock = DockStyle.Fill;
+            pb.Location = new Point(0, 0);
+            pb.Name = "pb";
+            pb.Size = new Size(800, 450);
+            pb.TabIndex = 0;
+            pb.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pb);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pb).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pb;
     }
 }
---
+++ PongLibrary/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PongLibrary
+{
+    public class Player
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Player(int x, int y, int w, int h)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Width = w;
+            this.Height = h;
+        }
+    }
+}
